f65a63ee68bc046c021cd42ca3e61cafeb5a643f
German <schacht.german@gmail.com>
2015-02-06 10:54:56 -0300

    Added list of connected users, and more comments to the code

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
+// Iniciar el server con express y socketIO
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Agregar una carpeta como carpeta pública
 app.use(express.static(__dirname + '/public'));
 
+// Responder con el html cuando se accede a la ruta '/'
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+// iniciar arreglos de usuarios, posición X e Y
 var left = [];
 var top = [];
 var users = [];
 
+// iniciar arreglo de sockets
 var sockets = []
 
+// captura de eventos por parte del server
 io.on('connection', function(socket){
+
+	// cuando alguien se conecta, se envía todas las ubicaciones de los usuarios conectados
 	socket.emit('inicio', users, left, top);
-	console.log('un usuario se ha conectado');
+
+	// mostrar mje por pantalla del evento
+	console.log('[INFO - CONNECT] un usuario se ha conectado');
+	
+	// cuando el usuario se registro con su nickname
 	socket.on('ingresar', function(name) {
+		
+		// agregar al usuario en el arreglo
 		users.push(name);
+		
+		// iniciar coordenadas 50-50
 		top.push('50px');
 		left.push('50px');
+
+		// agregar al socket en el arreglo
 		sockets.push(socket);
-		console.log(name + ' se ha conectado!');
+
+		// mostrar mje por consola del evento
+		console.log('[INFO - CONNECT] "' + name + '" se ha conectado!');
+
+		// difundir evento de nuevo usuario conectado a todos los demas usuarios
 		socket.broadcast.emit('nuevo user', name);
 	});
+
+	// cuando un usuario se mueve guardar las nuevas coordenadas
 	socket.on('moviendo div', function(user, rLeft, rTop) {
 		var j = users.indexOf(user);
 		left[j] = rLeft;
 		top[j] = rTop;
+
+		// difundir las coordenadas a los demás usuarios
 		socket.broadcast.emit('moviendo div', user, rLeft, rTop);
 	});
+
+	// cuando un usuario se desconecta
 	socket.on('disconnect', function() {
+
+		// obtener usuario desconectado
 		var index = sockets.indexOf(socket);
 		var muerto = users[index];
-		console.log(muerto +' desconectado');
+
+		// mostrar por consola el mje de desconexión
+		console.log('[INFO - DISCONNECT] "' + muerto +'" desconectado');
+
+		// difundir mje a todos los usuarios que se ha desconectado el usuario
 		socket.broadcast.emit('div muerto', muerto);
+		
+		// quitar usuario de todos los arreglos
 		if (index > -1) {
 		    sockets.splice(index, 1);
 		    users.splice(index, 1);
@@ -46,6 +82,7 @@ io.on('connection', function(socket){
 	})
 });
 
+// iniciar el server en el puerto designado
 http.listen(3000, function(){
-	console.log('escuchando en *:3000');
+	console.log('[INFO] escuchando en *:3000');
 });
--- public/js/perro.js
+++ public/js/perro.js
@@ -1,62 +1,102 @@
+// Inicializar sockets
 var socket = io();
 
-var nickname;
+var nickname; // Nombre propio
+var conectados = []; // Nombre de los jugadores
 
-var conectados = [];
-
+// Ocultar error y pantalla principal
 $('div#display').hide();
 $('#error').hide();
 
+// Cuando se ingresa el nombre de usuario en el formulario de la pantalla principal
 $('form#login').submit(function() {
+	
+	// Obtener nickname y eliminar los espacios
 	var nickname = $('input#nickInput').val();
 	nickname = nickname.replace(/ /g,'-');
+	
+	// Controlar que no exista nadie con el mismo nickname y mostrar error
 	if (conectados.indexOf(nickname) !== -1) {
 		$('#error').slideDown(200);
 		return false;
 	};
+
+	// Pasar a pantalla principal (quitando el error si corresponde)
 	$('#error').slideUp(200)
 	$('div#nickname').slideUp(200);
 	$('div#display').slideDown(200);
+	
+	// Avisar al server que se ingreso al juego
 	socket.emit('ingresar', nickname);
 
+	// Agregar el div propio para jugar
 	$('div#display').append($('<div style="z-index:999; background:darkcyan;" class="divcito" id="'+ nickname +'"><div id="text"><h3>'+nickname+'</h3><p>Haceme clic</p><p>y arrastrá</p></div></div>'));
 
+	// Darle movimiento con el click y arrastrar
 	$('#'+nickname).mousemove(function(e) {
 		if (e.which === 1) {
-			$('#'+nickname).css('left',e.pageX - 100).css('top',e.pageY - 100);
+			$('.divcito#'+nickname).css('left',e.pageX - 100).css('top',e.pageY - 100);
+			
+			// emitir evento de movimiento
 			socket.emit('moviendo div', nickname,$('#'+nickname).css('left'), $('#'+nickname).css('top'));
 		};
 
 	});
 
+	// Agregarse a sí mismo a la lista de usuarios conectados
+	$('ul#usuariosUl').append($('<li id="tu"></li>').text(nickname));
+
+	// Para que no realize el post
 	return false;
 });
 
-
+// Recepción de todos los otros jugadores por parte del servidor
 socket.on('inicio', function(users, left, top) {
+	
+	// Llenar la lista de demás jugadores
 	conectados = users;
+
+	// Agregar un div por cada jugador y posicionarlo en la ubicación actual
 	for (var i = 0; i < users.length; i++) {
 		$('div#display').append($('<div class="divcito" id="'+ users[i] +'"><div id="text"><h3>'+users[i]+'</h3><p>Se mueve solo</p><p>:)</p></div></div>'));
-		$('#'+users[i]).css('left',left[i]).css('top',top[i]);
+		$('.divcito#'+users[i]).css('left',left[i]).css('top',top[i]);
+
+		//agregar a la lista de conectados
+		$('ul#usuariosUl').append($('<li></li>').text(users[i]));
 	};
 });
 
 //nuevo usuario conectado
 socket.on('nuevo user', function(user) {
+	
+	// Agregar nuevo usuario a la lista de conectados
 	conectados.push(user);
+
+	// Agregar div de nuevo usuario
 	$('div#display').append($('<div class="divcito" id="'+ user +'"><div id="text"><h3>'+user+'</h3><p>Se mueve solo</p><p>:)</p></div></div>'));
-	$('#'+user).css('left','50px').css('top','50px');
+	$('.divcito#'+user).css('left','50px').css('top','50px');
+
+	// Agregar a la lista de usuarios conectados
+	$('ul#usuariosUl').append($('<li></li>').text(user));
 });
 
-
-//moviendo div
+// Evento de movimientos de otros usuarios
 socket.on('moviendo div', function(user, left,top) {
-	$('#'+user).css('left',left).css('top',top);
+	
+	// asignar la nueva posición del div correspondiente
+	$('.divcito#'+user).css('left',left).css('top',top);
 });
 
-//capturar quien sigue conectado
+// Desconexión de un usuario
 socket.on('div muerto', function(user) {
-	$('#'+user).remove();
+	
+	// Quitar div de usuario desconectado
+	$('.divcito#'+user).remove();
+
+	// Quitar usuario de la lista de usuarios conectados
+	$('ul#usuariosUl li').filter(function(index){ return $(this).text() === user; }).remove();
+	
+	// Eliminar el usuario de la lista interna de usuarios
 	var index = conectados.indexOf(user);
 	if (index > -1) {
 	    conectados.splice(index, 1);
